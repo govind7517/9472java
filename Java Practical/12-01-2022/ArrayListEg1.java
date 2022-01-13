3ab297f545629dfb6c63853ff2cf6cfcdcb1d6a0
govind7517 <95117104+govind7517@users.noreply.github.com>
2022-01-13 06:40:46 +0530

    Add files via upload

---
+++ Java Practical/12-01-2022/ArrayListEg1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ArrayListEg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		  List<String> list_Strings = new ArrayList<String>();
+		  
+		  list_Strings.add("White");
+		  list_Strings.add("Green");
+		  list_Strings.add("Orange");
+		  list_Strings.add("Red");
+		  list_Strings.add("Black");
+		  
+		  System.out.println(list_Strings);
+	}
+
+}
