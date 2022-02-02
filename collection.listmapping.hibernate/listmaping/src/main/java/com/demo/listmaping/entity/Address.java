aeaa2d24526b5d69546c3a271b7b6d56f1d28c11
govind7517 <95117104+govind7517@users.noreply.github.com>
2022-02-02 19:37:47 +0530

    Add files via upload

---
+++ collection.listmapping.hibernate/listmaping/src/main/java/com/demo/listmaping/entity/Address.java
@@ -0,0 +1,37 @@
+package com.demo.listmaping.entity;
+import javax.persistence.*;
+@Embeddable
+public class Address {
+	private int e_pincode;
+	
+	public Address() {}
+	public Address(int e_pincode, String e_city, String e_state) {
+		super();
+		this.e_pincode = e_pincode;
+		this.e_city = e_city;
+		this.e_state = e_state;
+	}
+	private String e_city;
+	public int getE_pincode() {
+		return e_pincode;
+	}
+	public void setE_pincode(int e_pincode) {
+		this.e_pincode = e_pincode;
+	}
+	public String getE_city() {
+		return e_city;
+	}
+	public void setE_city(String e_city) {
+		this.e_city = e_city;
+	}
+	public String getE_state() {
+		return e_state;
+	}
+	public void setE_state(String e_state) {
+		this.e_state = e_state;
+	}
+	private String e_state;
+
+	
+
+}
